6c777ee08c39b24597100cbe0222cdf0cd3b74e6
Marcus Ong <ong1.marcusb@gmail.com>
2026-04-22 22:47:49 +0800

    :sparkles: Setup API handlers for writing logs and routes for it

---
+++ logger-service/cmd/api/routes.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func (app *Config) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// specify who is allowed to connect
+	mux.Use(cors.Handler(cors.Options{
+		// dev mode, allow all connection
+		AllowedOrigins:   []string{"https://*", "http://*"}, // allows all connections from all domains
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"}, // cross site request forgery token
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true, // deal with credential requests
+		MaxAge:           300,
+	}))
+
+	// Make sure service is alive by checking it's heartbeat route
+	mux.Use(middleware.Heartbeat("/ping"))
+
+	// Write a POST request handler to our / endpoint
+	mux.Post("/log", app.WriteLog)
+
+	// add routes
+	return mux
+}
