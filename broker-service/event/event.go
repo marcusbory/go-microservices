ad1fe247e60bc77716482ddc1f720bffb330beab
Marcus Ong <ong1.marcusb@gmail.com>
2026-04-24 15:03:51 +0800

    :zap: Remove declare random queue for handling event consumption (not needed for broker

--- broker-service/event/event.go
+++ broker-service/event/event.go
@@ -4,6 +4,9 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+// ? Ensure RabbitMQ has a topic exchange named 'logs_topic' with such properties.
+// Create a topic exchange in RabbitMQ (if not existing)
+// * "Setting up a shared mailroom - publisher drops off mail, consumer delivers to right places"
 func declareExchange(ch *amqp.Channel) error {
 	return ch.ExchangeDeclare(
 		"logs_topic", // name
@@ -16,13 +19,14 @@ func declareExchange(ch *amqp.Channel) error {
 	)
 }
 
-func declareRandomQueue(ch *amqp.Channel) (amqp.Queue, error) {
-	return ch.QueueDeclare(
-		"",    // name? pick your own or auto generate
-		false, // durable, get rid when done with it
-		false, // delete when unused
-		true,  // exclusive, don't share queue
-		false, // no-wait
-		nil,   // arguments
-	)
-}
+// ! Not needed for a Publisher (Broker), only Consumer (Listener)
+// func declareRandomQueue(ch *amqp.Channel) (amqp.Queue, error) {
+// 	return ch.QueueDeclare(
+// 		"",    // name? pick your own or auto generate
+// 		false, // durable, get rid when done with it
+// 		false, // delete when unused
+// 		true,  // exclusive, don't share queue
+// 		false, // no-wait
+// 		nil,   // arguments
+// 	)
+// }
